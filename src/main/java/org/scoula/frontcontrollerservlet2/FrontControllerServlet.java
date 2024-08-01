ebc19011a7c28297bc54304fb2d6300bd3e88387
Park Hyewon <db1648@naver.com>
2024-08-01 14:42:24 +0900

    240801 FrontControllerServlet 생성

--- src/main/java/org/scoula/frontcontrollerservlet2/FrontControllerServlet.java
+++ src/main/java/org/scoula/frontcontrollerservlet2/FrontControllerServlet.java
@@ -1,26 +1,38 @@
 package org.scoula.frontcontrollerservlet2;
+
+
+import org.scoula.frontcontrollerservlet2.controller.Controller;
+
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.WebServlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
 
-@WebServlet("/")
+@WebServlet(name="FrontControllerServlet",value="/")
 public class FrontControllerServlet extends HttpServlet {
+    private Map<String, Controller> controllers = new HashMap<>();
+
+    @Override
+    public void init() throws ServletException {
+        //주소값과 컨트롤러 맵핑
+
+    }
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        response.setContentType("text/html;charset=utf-8");
-
-        PrintWriter out = response.getWriter();
-        out.println("<html><body>");
-        out.println("");
-        out.println("</body></html>");
+        processRequest(request, response);
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        doGet(request, response);
+        processRequest(request, response);
+
+    }
+    private void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
     }
 }
